ef7bea1b16b47e34f9cb36f3d8ec8433c0b687cd
Raymond Maung <raymaung@gmail.com>
2018-03-05 15:01:07 +1100

    chapter-4 Guess Cooridinate

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -77,6 +77,27 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state_data) do
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state_data, opponent_key)
+
+    with {:ok, rules} <- Rules.check(state_data.rules, {:guess_coordinate, player_key}),
+      {:ok, coordinate} <- Coordinate.new(row, col),
+      {hit_or_miss, forested_island, win_status, opponent_board} <- Board.guess(opponent_board, coordinate),
+      {:ok, rules} <- Rules.check(rules, {:win_check, win_status}) do
+        state_data
+        |> update_board(opponent_key, opponent_board)
+        |> update_guesses(player_key, hit_or_miss, coordinate)
+        |> update_rules(rules)
+        |> reply_success({hit_or_miss, forested_island, win_status})
+      else
+        :error ->
+          {:reply, :error, state_data}
+        {:error, :invalid_coordinate} ->
+          {:reply, {:error, :invalid_coordinate}, state_data}
+      end
+  end
+
   defp update_board(state_data, player, board) do
     Map.update!(state_data, player, fn player -> %{player | board: board} end)
   end
@@ -101,6 +122,18 @@ defmodule IslandsEngine.Game do
     GenServer.call(game, {:set_islands, player})
   end
 
+  def guess_coordinate(game, player, row, col) when player in @players do
+    GenServer.call(game, {:guess_coordinate, player, row, col})
+  end
+
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
+
+  defp update_guesses(state_data, player_key, hit_or_miss, coordinate) do
+    update_in(state_data[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
 
   # def demo_call(game) do
   #   GenServer.call(game, :demo_call)
--- test/islands_engine/game_test.exs
+++ test/islands_engine/game_test.exs
@@ -1,10 +1,27 @@
 defmodule IslandsEngine.GameTest do
   use ExUnit.Case
 
-  alias IslandsEngine.Game
+  alias IslandsEngine.{Game, Rules}
 
   @tag :wip
-  test "xxx" do
+  test "verify guess coordinate" do
+    {:ok, game} = Game.start_link("Miles")
+    :error = Game.guess_coordinate(game, :player1, 1, 1)
+    :ok = Game.add_player(game, "Trane")
+    :ok = Game.position_island(game, :player1, :dot, 1, 1)
+    :ok = Game.position_island(game, :player2, :square, 1, 1)
+    state_data = :sys.get_state(game)
+
+    state_data = :sys.replace_state(game, fn data ->
+      %{state_data | rules: %Rules{state: :player1_turn}}
+    end)
+
+    {:miss, :none, :no_win} = Game.guess_coordinate(game, :player1, 5, 5)
+    :error = Game.guess_coordinate(game, :player1, 3, 1)
+    {:hit, :dot, :win} = Game.guess_coordinate(game, :player2, 1, 1)
+  end
+
+  test "verify set_islands" do
     {:ok, game} = Game.start_link("Dino")
     Game.add_player(game, "Pebbles")
 
@@ -16,6 +33,6 @@ defmodule IslandsEngine.GameTest do
     :ok = Game.position_island(game, :player1, :s_shape, 5, 1)
     :ok = Game.position_island(game, :player1, :square, 5, 5)
 
-    assert {:ok, %{atoll: _}} = Game.set_islands(game, :player1)
+    {:ok, %{atoll: _}} = Game.set_islands(game, :player1)
   end
 end
